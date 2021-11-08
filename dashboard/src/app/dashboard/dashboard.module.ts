c664ac45f706fd55b48b776f9e602afb0f4a2270
Chanchaljangir <chanchaljangir9@gmail.com>
2021-11-08 16:06:29 +0530

    changes

--- dashboard/src/app/dashboard/dashboard.module.ts
+++ dashboard/src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
+import { dashboardRoutingModule } from './dashboard-routing.module';
 
 
 
 @NgModule({
   declarations: [HomeComponent, DetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    dashboardRoutingModule
   ]
 })
 export class DashboardModule { }
